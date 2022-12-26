ff9f3c80a27f65f28b9e06b4dec69c56491ad99c
Marius Butuc <marius.butuc@gmail.com>
2022-12-26 16:50:54 -0500

    Link dockSettings

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -25,6 +25,11 @@
       source = ./config/direnv.toml;
       target = ".config/direnv/direnv.toml";
     };
+    dockSettings = {
+      executable = false;
+      source = ./config/com.apple.dock.plist;
+      target = "~/Library/Preferences/com.apple.dock.plist";
+    };
     sshConfig = {
       executable = false;
       source = ./config/ssh;
